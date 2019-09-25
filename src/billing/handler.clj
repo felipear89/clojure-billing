5859275a44cb57ed0e0706409a98da53f8152c9a
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2019-09-25 00:21:18 -0300

    update compojure handler to use api-defaults

--- src/billing/handler.clj
+++ src/billing/handler.clj
@@ -1,11 +1,11 @@
 (ns billing.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes api-defaults))
