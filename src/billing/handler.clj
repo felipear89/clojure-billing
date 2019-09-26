fbc226f198ad620795f3deab906925faf54057c7
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2019-09-25 23:12:09 -0300

    wrap response on data

--- src/billing/handler.clj
+++ src/billing/handler.clj
@@ -9,15 +9,23 @@
   (GET "/" [] "Hello World")
   (GET "/customers" []  {:body (db/get-customers)})
   (GET "/default_rates" [] {:body (db/get-default-rates)})
-  (POST "/calc" request (str (:body request)))
+  (POST "/calc" request {:body request})
   (route/not-found "Not Found"))
 
 (defn wrap-content-json [h]
-  (fn [req] (assoc-in (h req) [:headers "Content-Type"] "application/json")))
+  (fn [req] (assoc-in (h req) [:headers "Content-Type"] "application/json; charset=utf-8")))
+
+(defn wrap-json-data [h]
+  (fn [req]
+    (-> (h req)
+        (update-in [:body] (fn [b] {"data" b}))
+        )
+    ))
 
 (def app
   (->	(wrap-defaults	app-routes	api-defaults)
       (wrap-json-body)
+       (wrap-json-data)
        (wrap-json-response)
        (wrap-content-json)
        ))
